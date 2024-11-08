cfad3cf093ed36f03d7e78234ae5b03e0ed2d37e
laura <laura@LAPTOP-O1262475>
2024-11-08 17:18:05 +0100

    pl1_netbeans

---
+++ src/main/java/poo/pl1pdp/Pl1PdP.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pl1pdp;
+
+public class Pl1PdP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
